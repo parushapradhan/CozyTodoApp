e668c8e2f61aa747ae08fe23f2d1f71192450caa
parusha <parusha.pradhan@gmail.com>
2025-04-01 19:41:36 -0400

    clean up

---
+++ ROLEWORLD_WIZARD_INTERIOR TILESET.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="ROLEWORLD_WIZARD_INTERIOR TILESET" tilewidth="32" tileheight="32" tilecount="20" columns="5">
+ <image source="../../Desktop/ROLEWORLD WIZARD/Interior/ROLEWORLD_WIZARD_INTERIOR TILESET.png" width="176" height="144"/>
+</tileset>
